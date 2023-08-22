960ce719005c2fa3714bb5bbd4e7aaf5c4cdade9
Sannusharma02 <sannusharma02@gmail.com>
2023-08-22 17:26:26 +0530

    m

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-
+import "./index.css"
 
 const pizzaData = [
     {
@@ -47,24 +47,60 @@ const pizzaData = [
     },
 ];
 
-function App(){
-    return <div>
-        <h1>Hello React</h1>
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
+function App() {
+    return (
+        <div className="container">
+            <Header />
+            <Menu />
+            <Footer />
+        </div>)
+}
+
+function Header() {
+    const style = {};
+
+    return <header className="header">
+        <h1 style={style}>
+            Fast React Pizza Co.
+        </h1>
+    </header>
+}
+
+function Menu() {
+    return <main className="menu">
+        <h2> Our Menu </h2>
+        <Pizza
+            name='Pizza Spinaci'
+            ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+            photoName='pizzas/spinaci.jpg'
+            price={10} />
+
+        <Pizza
+            name="Pizza Funghi"
+            ingredients="Tomato, mozarella, mushrooms, and onion"
+            photoName='pizzas/funghi.jpg'
+            price={12} />
+    </main>
+}
+
+function Pizza(props) {
+
+    return <div className="pizza">
+        <img src={props.photoName} alt={props.name} />
+        <div>
+            <h3>{props.name}</h3>
+            <p>{props.ingredients}</p>
+            <span>{props.price + 3}</span>
+        </div>
+
     </div>
 }
 
-function Pizza(){
-    return (
-        <div>
-        <img src='./pizzas/spinaci.jpg' alt='Pizza Spinaci' />
-        <h2>Pizza Spinaci</h2>
-        <p>Tomato, mozzarella, spinach, and ricotta cheese</p>
-    </div>
-    );
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+
+    return (<footer>{new Date().toLocaleDateString()}.We're currently open!</footer>)
 }
 
 ReactDom.createRoot(document.getElementById("root")).render(<React.StrictMode>< App /></React.StrictMode>);
